b902ca8d05831fc43d5a4e1ccd4ef0c77c6cff70
mangx <771854332@qq.com>
2017-09-17 00:25:59 +0800

    回退

--- src/components/UserTab/Basic.js
+++ src/components/UserTab/Basic.js
@@ -1,17 +1,22 @@
 // @flow
 import React from 'react'
 import styles from './Basic.css'
-import { Table, Icon, Form, Input, Select, Modal, Row, Col, message } from 'antd'
+import { Table, Icon, Input, Select, Modal, Row, Col, message } from 'antd'
 const Option = Select.Option
-const FormItem = Form.Item
 import universalFetch, { handleFetchError } from '../../utils/fetch'
 
 type Props = {
-  data: Object,
-  form: Object
+  data: Object
 }
 type States = {
   dataSource: Array<Object>,
+  cxpd: string,
+  dy: string,
+  wt: string,
+  xsky: string,
+  post: string,
+  xskybz: string,
+  postbz: string,
   visible: boolean,
   modalData: Object
 }
@@ -22,52 +27,58 @@ class Basic extends React.Component {
     super(props)
     this.state = {
       dataSource: [],
+      cxpd: '',
+      dy: '',
+      wt: '',
+      xsky: '',
+      post: '',
       modalData: {},
-      visible: false
+      visible: false,
+      xskybz: '',
+      postbz: ''
     }
   }
   getTableData = () => {
     const { data } = this.props
     const updateScore = this.updateScore
     universalFetch(`${__API__}base-score/${data.classId}`)
-    .then(res => res.json())
-    .then((json) => {
-      console.log(json)
-      if (json.code !== 0) {
-        throw new Error(JSON.stringify(
-          {
-            code: json.code,
-            message: json.message
-          }
-        ), 'Basic.js')
-      }
-      this.setState({
-        dataSource: json.data.map((item, index) => {
-          return {
-            key: index,
-            num: item.studentNumber,
-            name: item.name,
-            cxpk: item.behavior,
-            dy: item.moral,
-            wt: item.activity,
-            xsky: item.academic + (item.academicDesc ? `(${item.academicDesc})` : ''),
-            post: item.duty + (item.dutyDesc ? `(${item.dutyDesc})` : ''),
-            operate: <span className={styles['operate']} onClick={function () { updateScore(item) }}>
-              <Icon type='edit' />修改</span>
-          }
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json)
+        if (json.code !== 0) {
+          throw new Error(JSON.stringify(
+            {
+              code: json.code,
+              message: json.message
+            }
+          ), 'Basic.js')
+        }
+        this.setState({
+          dataSource: json.data.map((item, index) => {
+            return {
+              key: index,
+              num: item.studentNumber,
+              name: item.name,
+              cxpk: item.behavior,
+              dy: item.moral,
+              wt: item.activity,
+              xsky: item.academic + (item.academicDesc ? `(${item.academicDesc})` : ''),
+              post: item.duty + (item.dutyDesc ? `(${item.dutyDesc})` : ''),
+              operate: <span className={styles['operate']} onClick={function () { updateScore(item) }}>
+                <Icon type='edit' />修改</span>
+            }
+          })
         })
       })
-    })
-    .catch(handleFetchError)
+      .catch(handleFetchError)
   }
   handleCancel = () => {
     this.setState({
       visible: false
     })
   }
-  handleOk = (values) => {
-    const { modalData } = this.state
-    const { cxpd, dy, wt, xsky, post, xskybz, postbz } = values
+  handleOk = () => {
+    const { cxpd, dy, wt, xsky, post, xskybz, postbz, modalData } = this.state
     const data = {
       systemId: modalData.systemId,
       moral: dy,
@@ -78,7 +89,6 @@ class Basic extends React.Component {
       duty: post,
       dutyDesc: postbz
     }
-    console.log('data', data)
     universalFetch(`${__API__}base-score`, {
       method: 'PUT',
       headers: {
@@ -86,24 +96,34 @@ class Basic extends React.Component {
       },
       body: JSON.stringify(data)
     })
-    .then(res => res.json())
-    .then((json) => {
-      console.log(json)
-      if (json.code !== 0) {
-        throw new Error(JSON.stringify(
-          {
-            code: json.code,
-            message: json.message
-          }
-        ), 'Basic.js')
-      }
-      this.getTableData()
-      this.setState({
-        visible: false
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json)
+        if (json.code !== 0) {
+          throw new Error(JSON.stringify(
+            {
+              code: json.code,
+              message: json.message
+            }
+          ), 'Basic.js')
+        }
+        this.getTableData()
+        this.setState({
+          visible: false
+        })
+        message.success('修改成功')
       })
-      message.success('修改成功')
+      .catch(handleFetchError)
+  }
+  changeInput = (e: Object) => {
+    this.setState({
+      [e.target.name]: e.target.value
     })
-    .catch(handleFetchError)
+  }
+  changeCxpd = (value: string) => {
+    this.setState({
+      cxpd: value
+    })
   }
   componentWillMount () {
     this.getTableData()
@@ -111,50 +131,49 @@ class Basic extends React.Component {
   updateScore = (item: Object) => {
     this.setState({
       visible: true,
-      modalData: item
-    })
-  }
-  submitHandle = () => {
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values)
-        this.handleOk(values)
-      }
+      modalData: item,
+      cxpd: item.behavior,
+      dy: item.moral,
+      wt: item.activity,
+      xsky: item.academic,
+      xskybz: item.academicDesc,
+      post: item.duty,
+      postbz: item.dutyDesc
     })
   }
   getData = () => {
     const { data } = this.props
     const updateScore = this.updateScore
     universalFetch(`${__API__}base-score/${data.classId}`)
-    .then(res => res.json())
-    .then((json) => {
-      console.log(json)
-      if (json.code !== 0) {
-        throw new Error(JSON.stringify(
-          {
-            code: json.code,
-            message: json.message
-          }
-        ), 'Basic.js')
-      }
-      this.setState({
-        dataSource: json.data.map((item, index) => {
-          return {
-            key: index,
-            num: item.studentNumber,
-            name: item.name,
-            cxpd: item.behavior,
-            dy: item.moral,
-            wt: item.activity,
-            xsky: item.academic + `(${item.academicDesc})`,
-            post: item.duty + `(${item.dutyDesc})`,
-            operate: <span className={styles['operate']} onClick={function () { updateScore(item) }}>
-              <Icon type='edit' />修改</span>
-          }
+      .then(res => res.json())
+      .then((json) => {
+        console.log(json)
+        if (json.code !== 0) {
+          throw new Error(JSON.stringify(
+            {
+              code: json.code,
+              message: json.message
+            }
+          ), 'Basic.js')
+        }
+        this.setState({
+          dataSource: json.data.map((item, index) => {
+            return {
+              key: index,
+              num: item.studentNumber,
+              name: item.name,
+              cxpd: item.behavior,
+              dy: item.moral,
+              wt: item.activity,
+              xsky: item.academic + `(${item.academicDesc})`,
+              post: item.duty + `(${item.dutyDesc})`,
+              operate: <span className={styles['operate']} onClick={function () { updateScore(item) }}>
+                <Icon type='edit' />修改</span>
+            }
+          })
         })
       })
-    })
-    .catch(handleFetchError)
+      .catch(handleFetchError)
   }
   render () {
     const columns = [{
@@ -190,157 +209,93 @@ class Basic extends React.Component {
       dataIndex: 'operate',
       key: 'operate'
     }]
-    const { dataSource, modalData } = this.state
-    const { getFieldDecorator, getFieldValue } = this.props.form
-    const flag1 = !(getFieldValue('xsky') === '' || getFieldValue('xsky') === '0')
-    const flag2 = !(getFieldValue('post') === '' || getFieldValue('post') === '0')
+    const { dataSource, cxpd, dy, wt, xsky, post, xskybz, postbz, modalData } = this.state
     return <div className={styles['main']}>
       <Table columns={columns} pagination={false} dataSource={dataSource} />
       <Modal
         title='修改基本加分'
         visible={this.state.visible}
-        onOk={this.submitHandle}
+        onOk={this.handleOk}
         onCancel={this.handleCancel}
         maskClosable={false}
         width={600}
-        >
-        <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginBottom: 15 }}>
-          <div className={styles['row']}>
-            <span className={styles['title']}>姓名：</span>
-            {modalData.name}
-          </div>
-          <div className={styles['row']}>
-            <span className={styles['title']}>学号：</span>
-            {modalData.studentNumber}
-          </div>
-        </div>
+      >
         <div className={styles['modal-main']}>
-          <Form>
-            <FormItem
-              labelCol={{ xs: { span: 5 } }}
-              label={'操行评定：'}>
-              {getFieldDecorator('cxpd', {
-                rules: [{ required: true, message: '请输入操行评定!' }],
-                initialValue: modalData.behavior
-              })(
-                <Select style={{ width: '150px' }} placeholder='新选择操行评定'>
-                  <Option value='优'>优</Option>
-                  <Option value='良'>良</Option>
-                  <Option value='中'>中</Option>
-                  <Option value='差'>差</Option>
-                </Select>
-              )}
-            </FormItem>
-            <Row>
-              <Col span={12}>
-                <div className={styles['row']}>
-                  <FormItem
-                    {...formItemLayout}
-                    label={'德育：'}>
-                    {getFieldDecorator('dy', {
-                      rules: [{ required: true, message: '请输入德育分!' }],
-                      initialValue: modalData.moral
-                    })(
-                      <Input style={{ width: '150px' }} placeholder='请输入德育分' />
-                    )}
-                  </FormItem>
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <div className={styles['row']}>
-                  <FormItem
-                    {...formItemLayout}
-                    label={'学术科研：'}>
-                    {getFieldDecorator('xsky', {
-                      rules: [{ required: true, message: '请输入学术科研分!' }],
-                      initialValue: modalData.academic
-                    })(
-                      <Input style={{ width: '150px' }} placeholder='请输入学术科研分' />
-                    )}
-                  </FormItem>
-                </div>
-              </Col>
-              <Col span={12}>
-                <div className={styles['row']}>
-                  <FormItem
-                    key={'xskybz' + flag1.toString()}
-                    {...formItemLayout}
-                    label={'学术科研备注：'}>
-                    {getFieldDecorator('xskybz', {
-                      rules: [{
-                        required: flag1,
-                        max: 50,
-                        message: '请确认学术科研备注!' }],
-                      initialValue: !flag1
-                          ? '' : modalData.academicDesc
-                    })(
-                      <Input style={{ width: '150px' }}
-                        disabled={!flag1}
-                        placeholder='请输入学术科研备注' />
-                    )}
-                  </FormItem>
-                </div>
-              </Col>
-            </Row>
-            <Row>
-              <Col span={12}>
-                <div className={styles['row']}>
-                  <FormItem
-                    {...formItemLayout}
-                    label={'职务：'}>
-                    {getFieldDecorator('post', {
-                      rules: [{ required: true, message: '请输入职务分!' }],
-                      initialValue: modalData.duty
-                    })(
-                      <Input style={{ width: '150px' }} placeholder='请输入职务分' />
-                    )}
-                  </FormItem>
-                </div>
-              </Col>
-              <Col span={12}>
-                <div className={styles['row']}>
-                  <FormItem
-                    key={'postbz' + flag2.toString()}
-                    {...formItemLayout}
-                    label={'职务分备注：'}>
-                    {getFieldDecorator('postbz', {
-                      rules: [{
-                        required: flag2,
-                        max: 20,
-                        message: '请确认职务分备注!' }],
-                      initialValue: !flag2 ? '' : modalData.dutyDesc
-                    })(
-                      <Input style={{ width: '150px' }}
-                        disabled={!flag2}
-                        placeholder='请输入职务分备注' />
-                    )}
-                  </FormItem>
-                </div>
-              </Col>
-            </Row>
-          </Form>
+          <Row>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>姓名：</span>
+                {modalData.name}
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>学号：</span>
+                {modalData.studentNumber}
+              </div>
+            </Col>
+          </Row>
+          <div className={styles['row']}>
+            <span className={styles['title']}>操行评定：</span>
+            <Select style={{ width: '150px' }} placeholder='新选择操行评定'
+              onChange={this.changeCxpd} value={cxpd}>
+              <Option value='优'>优</Option>
+              <Option value='良'>良</Option>
+              <Option value='中'>中</Option>
+              <Option value='差'>差</Option>
+            </Select>
+          </div>
+          <Row>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>德育：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入德育分'
+                  onChange={this.changeInput} value={dy} name='dy' />
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>文体：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入文体分'
+                  onChange={this.changeInput} value={wt} name='wt' />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>学术科研：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入学术科研分'
+                  onChange={this.changeInput} value={xsky} name='xsky' />
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>学术科研备注：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入学术科研备注'
+                  onChange={this.changeInput} value={xskybz} name='xskybz' />
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>职务：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入职务分'
+                  onChange={this.changeInput} value={post} name='post' />
+              </div>
+            </Col>
+            <Col span={12}>
+              <div className={styles['row']}>
+                <span className={styles['title']}>职务分备注：</span>
+                <Input style={{ width: '150px' }} placeholder='请输入职务分备注'
+                  onChange={this.changeInput} value={postbz} name='postbz' />
+              </div>
+            </Col>
+          </Row>
         </div>
       </Modal>
     </div>
   }
 }
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 10 }
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 10 }
-  }
-}
-const newBasic = Form.create()(Basic)
-export default newBasic
+export default Basic
